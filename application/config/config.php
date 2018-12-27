5d4c11ac2a1700ea592e262c6b11558cd792bcd6
Dimas Rifqi <dimasrifqi296@gmail.com>
2018-12-27 12:45:30 +0700

    change base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://202.56.162.67:8080';
+$config['base_url'] = 'http://localhost';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://202.56.162.67:8080';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = 'index.php';
 
 /*
 |--------------------------------------------------------------------------
